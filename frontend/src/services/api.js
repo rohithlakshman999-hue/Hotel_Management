e8462cd8f9fb18909302e89bb607034832ce9824
neuralnexlabs22 <rohithlakshman999@gmail.com>
2026-04-06 11:43:20 +0530

    force fix api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  timeout: 10000, // accommodate longer local MongoDB transactions
+  baseURL: "https://hotel-management-hvth.onrender.com"
 });
 
 export default api;
